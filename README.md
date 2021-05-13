e65094e14d9add378e39f017dc7d05bbfe6eace4
Amira Ezzeldin <aezz4007@gmail.com>
2021-05-14 01:31:18 +0200

    feat: Create Navigation Menu
    Create navigation menu, use section title names, anchor menu to
    corresponding sections and create a fourth section.

--- README.md
+++ README.md
@@ -4,6 +4,7 @@
 
 * [Initial Code](#initial-code)
 * [Connect JavaScript file to HTML](#connect-javascript-file-to-html)
+* [Create Navigation Menu](#create-navigation-menu)
 
 ## Initial Code
 
@@ -19,5 +20,13 @@
 - Added `<script>` tag just before the ending `<body>` tag in HTML.
 - Tested the connection by writing some code to display a message in the console.
 - Deleted the test code, and comitted to Git.
+
+[Table of Contents](#table-of-contents)
+
+## Create Navigation Menu
+- Build the navigation menu using an array constant and a for loop.
+- Anchor the navigation bar items to their corresponding sections.
+- Populate the list items to display the section headings.
+- Add a new section to make sure the navigation bar will update accordingly.
 
 [Table of Contents](#table-of-contents)
--- js/app.js
+++ js/app.js
@@ -34,6 +34,27 @@
 */
 
 // build the nav
+//Add sections to an array
+const navBar=document.getElementsByTagName('section'); 
+//for loop to create navigation menu list elements for as many sections as the page has
+for (let i = 0; i < navBar.length; i++) {
+    //target the navigation bar list, to add list elements to it
+    const navBarList = document.getElementById('navbar__list')
+    //create list elements
+    newList = document.createElement('li');
+    //Add new list elements to its parent unordered list
+    navBarList.appendChild(newList);
+    //target section by id
+    const sections = document.getElementsByTagName('section')[i].id;
+   //target section by data-nav attribute
+    const myHeading = document.getElementsByTagName('section')[i].getAttribute('data-nav');
+     //Create an anchor tag
+    newList.innerHTML = `<a href="#${sections}"> ${myHeading} </a>`;
+    
+ }
+
+
+
 
 
 // Add class 'active' to section when near top of viewport
@@ -53,5 +74,3 @@
 // Scroll to section on link click
 
 // Set sections as active
-
-
