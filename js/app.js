1c9dabc7d2c300e30baf5f965b0c31c93c681b2b
Amira Ezzeldin <aezz4007@gmail.com>
2021-05-15 21:36:23 +0200

    feat: Active section styling
    Added active section styling functionality

--- js/app.js
+++ js/app.js
@@ -1,25 +1,25 @@
 // build the navigation bar
-function buildNavigation () {
-  //Add sections to an array
+
 const navBar=document.getElementsByTagName('section'); 
-//for loop to create navigation menu list elements for as many sections as the page has
 for (let i = 0; i < navBar.length; i++) {
-    //target the navigation bar list, to add list elements to it
     const navBarList = document.getElementById('navbar__list')
-    //create list elements
     newList = document.createElement('li');
-    //Add new list elements to its parent unordered list
     navBarList.appendChild(newList);
-    //target section by id
     const sections = document.getElementsByTagName('section')[i].id;
-   //target section by data-nav attribute
     const myHeading = document.getElementsByTagName('section')[i].getAttribute('data-nav');
-     //Create an anchor tag
     newList.innerHTML = `<a href="#${sections}" class="menu__link"> ${myHeading} </a>`;
+
+//set active section styling
+
+    if (navBar[i].getBoundingClientRect().top >= 0) {
+      navBar[i].classList.add('your-active-class');  
+    }
+      else {
+        navBar[i].classList.remove('your-active-class'); 
+      }
    
  }
-}
-//end of build navigation bar
 
-//function calls
-buildNavigation();
+
+
+
