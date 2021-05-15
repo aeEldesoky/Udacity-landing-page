a75b27f5f03d70320e16ce401a0f0c1e3da385b3
Amira Ezzeldin <aezz4007@gmail.com>
2021-05-15 19:26:35 +0200

    style: Added smooth scroll
    The html tag's scroll-behavior property was set to smooth
    in styles.css file.

--- js/app.js
+++ js/app.js
@@ -1,40 +1,6 @@
-/**
- * 
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- * 
- * Dependencies: None
- * 
- * JS Version: ES2015/ES6
- * 
- * JS Standard: ESlint
- * 
-*/
-
-/**
- * Define Global Variables
- * 
-*/
-
-
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
-
-
-
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
-
-// build the nav
-//Add sections to an array
+// build the navigation bar
+function buildNavigation () {
+  //Add sections to an array
 const navBar=document.getElementsByTagName('section'); 
 //for loop to create navigation menu list elements for as many sections as the page has
 for (let i = 0; i < navBar.length; i++) {
@@ -49,28 +15,11 @@ for (let i = 0; i < navBar.length; i++) {
    //target section by data-nav attribute
     const myHeading = document.getElementsByTagName('section')[i].getAttribute('data-nav');
      //Create an anchor tag
-    newList.innerHTML = `<a href="#${sections}"> ${myHeading} </a>`;
-    
+    newList.innerHTML = `<a href="#${sections}" class="menu__link"> ${myHeading} </a>`;
+   
  }
+}
+//end of build navigation bar
 
-
-
-
-
-// Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
-
-
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
-
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
+//function calls
+buildNavigation();
