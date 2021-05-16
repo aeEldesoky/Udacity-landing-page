e051bcb7477449d23e061f47c364ef17923390f3
Amira Ezzeldin <aezz4007@gmail.com>
2021-05-17 01:23:02 +0200

    fix: smooth scroll using JavaScript

--- js/app.js
+++ js/app.js
@@ -9,6 +9,7 @@ function buildNav () {
       const sections = document.getElementsByTagName('section')[i].id;
       const myHeading = document.getElementsByTagName('section')[i].getAttribute('data-nav');
       newList.innerHTML = `<a href="#${sections}" class="menu__link"> ${myHeading} </a>`;
+      document.getElementsByClassName('menu__link')[0].classList.add('active-link');
     }  
 }
 
@@ -16,14 +17,13 @@ function buildNav () {
 function activeSection () {
   document.addEventListener('scroll', function () {
     const sections=document.getElementsByTagName('section'); 
-  
     for (let i = 0; i < sections.length; i++) {
       let sectionTop = sections[i].getBoundingClientRect().top;
        if (sectionTop >= 0) {
-      if (sections[i].classList.contains('your-active-class') === false){
+         if (sections[i].classList.contains('your-active-class') === false){
         sections[i].classList.add('your-active-class');
-      }
-    }
+        }
+     }
     else {
       sections[i].classList.remove('your-active-class');
     }
@@ -32,6 +32,22 @@ function activeSection () {
   
 }
    
+// smooth scroll to section when link is clicked
+function smoothScroll () {
+  const sections=document.getElementsByTagName('section'); 
+  const links = document.getElementsByClassName('menu__link');
+  for (let i = 0; i < sections.length; i++) {
+links[i].addEventListener('click', function (e) {
+  e.preventDefault();
+  sections[i].scrollIntoView({behavior : 'smooth'});
+});
+  }
+}
+
+
+
+
 //function calls
 buildNav();
 activeSection();
+smoothScroll();
