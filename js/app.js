e5b67cb88582b9ef0bdd808d5171e66f9db066cf
Amira Ezzeldin <aezz4007@gmail.com>
2021-05-17 00:13:35 +0200

    fix: fixed the active section styling

--- js/app.js
+++ js/app.js
@@ -1,25 +1,37 @@
 // build the navigation bar
 
-const navBar=document.getElementsByTagName('section'); 
-for (let i = 0; i < navBar.length; i++) {
-    const navBarList = document.getElementById('navbar__list')
-    newList = document.createElement('li');
-    navBarList.appendChild(newList);
-    const sections = document.getElementsByTagName('section')[i].id;
-    const myHeading = document.getElementsByTagName('section')[i].getAttribute('data-nav');
-    newList.innerHTML = `<a href="#${sections}" class="menu__link"> ${myHeading} </a>`;
+function buildNav () {
+  const navBar=document.getElementsByTagName('section'); 
+  for (let i = 0; i < navBar.length; i++) {
+      const navBarList = document.getElementById('navbar__list')
+      newList = document.createElement('li');
+      navBarList.appendChild(newList);
+      const sections = document.getElementsByTagName('section')[i].id;
+      const myHeading = document.getElementsByTagName('section')[i].getAttribute('data-nav');
+      newList.innerHTML = `<a href="#${sections}" class="menu__link"> ${myHeading} </a>`;
+    }  
+}
 
 //set active section styling
-
-    if (navBar[i].getBoundingClientRect().top >= 0) {
-      navBar[i].classList.add('your-active-class');  
-    }
-      else {
-        navBar[i].classList.remove('your-active-class'); 
+function activeSection () {
+  document.addEventListener('scroll', function () {
+    const sections=document.getElementsByTagName('section'); 
+  
+    for (let i = 0; i < sections.length; i++) {
+      let sectionTop = sections[i].getBoundingClientRect().top;
+       if (sectionTop >= 0) {
+      if (sections[i].classList.contains('your-active-class') === false){
+        sections[i].classList.add('your-active-class');
       }
+    }
+    else {
+      sections[i].classList.remove('your-active-class');
+    }
+    }
+  });
+  
+}
    
- }
-
-
-
-
+//function calls
+buildNav();
+activeSection();
